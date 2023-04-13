9427cbd7a39b106d6e0d7e973349ae8bca41fe20
Shanuka Prabodha <shanukakariyawasam3@gmail.com>
2023-04-13 18:55:53 +0530

    product management crud operations completed

--- product/src/api/controllers/Product.controller.js
+++ product/src/api/controllers/Product.controller.js
@@ -1,7 +1,92 @@
-// import ProductService from "../services";
+ import ProductService from "../services";
+ const cloudinary=require('../../util/cloudinary');
 
-// TODO: Create a new product
-// TODO: Get all products
-// TODO: Get a product
-// TODO: Update a product
-// TODO: Delete a product
+
+ // Insert one product
+ export const insertProduct = async (request, response, next) => {
+
+    const{productId,productName,description, supplier,weight,variants}=request.body;
+     await ProductService.insertProduct(
+        {
+        productId,
+        productName,
+        description,
+        supplier,
+        weight,
+        variants
+     })
+         .then((data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
+ // Get all Product
+ export const getAllProducts = async (request, response, next) => {
+     await ProductService.getAllProducts("products")
+         .then(async (data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
+ // Get one Product
+ export const getOneProduct = async (request, response, next) => {
+     await ProductService.getOneProduct(request.params.id)
+         .then((data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
+ // Update one Product
+ export const updateProduct = async (request, response, next) => {
+     await ProductService.updateProduct(request.params.id, request.body)
+         .then((data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
+ // Delete one Product
+ export const deleteProduct = async (request, response, next) => {
+     await ProductService.deleteProduct(request.params.id)
+         .then((data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
+ // Search product
+ export const searchProducts = async (request, response, next) => {
+     await ProductService.searchProducts(request.params.search)
+         .then((data) => {
+             request.handleResponse.successRespond(response)(data);
+             next();
+         })
+         .catch((error) => {
+             request.handleResponse.errorRespond(response)(error.message);
+             next();
+         });
+ };
+ 
--- product/src/api/services/index.js
+++ product/src/api/services/index.js
@@ -1,5 +1,18 @@
-import {} from "./Product.service";
+import {
+	insertProduct,
+	getAllProducts,
+	getOneProduct,
+	updateProduct,
+	deleteProduct,
+	searchProducts,
+} from "./Product.service";
 
 export default {
 	// Product services
+	insertProduct,
+	getAllProducts,
+	getOneProduct,
+	updateProduct,
+	deleteProduct,
+	searchProducts,
 };
