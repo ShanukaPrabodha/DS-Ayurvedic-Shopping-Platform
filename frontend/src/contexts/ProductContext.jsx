06d702fc672491abffb7f0c3d0e3a98a7b8f4e90
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2023-04-16 18:38:04 +0530

    Update ProductContext.jsx

--- frontend/src/contexts/ProductContext.jsx
+++ frontend/src/contexts/ProductContext.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import ProductAPI from "./api/ProductAPI";
+import ProductAPI from "./api/ProductApi";
 
 const BASE_URL = "http://localhost:5003/api/product";
 // const BASE_URL = import.meta.env.VITE_K8S_BACKEND_URL;
