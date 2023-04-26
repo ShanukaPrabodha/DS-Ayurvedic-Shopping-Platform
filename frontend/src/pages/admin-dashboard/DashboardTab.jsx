833de1ae09a4d1cea16a49582c6df73a6b4cd88c
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2023-04-27 03:09:33 +0530

    Home Page Added

--- frontend/src/pages/admin-dashboard/DashboardTab.jsx
+++ frontend/src/pages/admin-dashboard/DashboardTab.jsx
@@ -9,6 +9,7 @@ const DashboardTab = () => {
 	const [totalRevenue, setTotalRevenue] = useState(0);
 	const [totalOrders, setTotalOrders] = useState(0);
 	const [totalProducts, setTotalProducts] = useState(0);
+	const [totalPendingOrders, setTotalPendingOrders] = useState(0);
 
 	// Get all products
 	const {
@@ -34,11 +35,12 @@ const DashboardTab = () => {
 				});
 			setTotalRevenue(total.toFixed(2));
 			setTotalOrders(orders.length);
+			setTotalPendingOrders(orders.filter((order) => order.status === "pending").length);
 		}
 	}, [orders]);
 
 	return (
-		<div className="bg-gray-100 h-screen">
+		<div className="bg-gray-100 h-screen rounded-lg p-6">
 			<div className="flex flex-wrap justify-between p-6">
 				<div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
 					<div className="bg-white rounded-lg shadow-lg p-6">
@@ -46,11 +48,13 @@ const DashboardTab = () => {
 							<div className="text-gray-600">Total Orders</div>
 							<div className="text-3xl font-bold">{totalOrders}</div>
 						</div>
-						<div className="mt-4">
-							<span className="text-green-500">
-								<i className="fas fa-arrow-up"></i> 15%
-							</span>
-							<span className="text-gray-600 ml-2">Since last week</span>
+					</div>
+				</div>
+				<div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
+					<div className="bg-white rounded-lg shadow-lg p-6">
+						<div className="flex items-center justify-between">
+							<div className="text-gray-600">Total Pending Orders</div>
+							<div className="text-3xl font-bold">{totalPendingOrders}</div>
 						</div>
 					</div>
 				</div>
@@ -60,39 +64,13 @@ const DashboardTab = () => {
 							<div className="text-gray-600">Total Revenue</div>
 							<div className="text-3xl font-bold">Rs. {totalRevenue}</div>
 						</div>
-						<div className="mt-4">
-							<span className="text-red-500">
-								<i className="fas fa-arrow-down"></i> 5%
-							</span>
-							<span className="text-gray-600 ml-2">Since last week</span>
-						</div>
 					</div>
 				</div>
-				{/* <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
-					<div className="bg-white rounded-lg shadow-lg p-6">
-						<div className="flex items-center justify-between">
-							<div className="text-gray-600">Total Users</div>
-							<div className="text-3xl font-bold">567</div>
-						</div>
-						<div className="mt-4">
-							<span className="text-green-500">
-								<i className="fas fa-arrow-up"></i> 10%
-							</span>
-							<span className="text-gray-600 ml-2">Since last week</span>
-						</div>
-					</div>
-				</div> */}
 				<div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4">
 					<div className="bg-white rounded-lg shadow-lg p-6">
 						<div className="flex items-center justify-between">
 							<div className="text-gray-600">Total Products</div>
 							<div className="text-3xl font-bold">{totalProducts}</div>
-						</div>
-						<div className="mt-4">
-							<span className="text-red-500">
-								<i className="fas fa-arrow-down"></i> 3%
-							</span>
-							<span className="text-gray-600 ml-2">Since last week</span>
 						</div>
 					</div>
 				</div>
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,7 +1,38 @@
 export default function Home() {
 	return (
 		<>
-			<h1 className="mt-5 text-4xl text-center">Home Page</h1>
+			<div className="relative bg-gray-800">
+				{/* Background Image */}
+				<div className="absolute inset-0">
+					<img
+						className="w-full h-full object-cover opacity-75"
+						src="https://images.unsplash.com/photo-1514733670139-4d87a1941d55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178&q=80"
+						alt="Background"
+					/>
+					<div className="absolute inset-0 bg-gray-800" style={{ mixBlendMode: "multiply" }}></div>
+				</div>
+
+				{/* Content */}
+				<div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<div className="py-24 md:py-32">
+						<h1 className="text-4xl font-extrabold text-white tracking-tight sm:text-5xl md:text-6xl">
+							Discover the Power of Herbs
+						</h1>
+						<p className="mt-6 max-w-3xl text-xl text-gray-300">
+							Explore our wide range of herbal products, from natural remedies to organic supplements, and unlock the
+							full potential of your body and mind.
+						</p>
+						<div className="mt-10">
+							<a
+								href="/product-display"
+								className="inline-block bg-primary-green py-3 px-8 border border-transparent rounded-md text-base font-medium text-white hover:bg-green-700"
+							>
+								Shop Now
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
