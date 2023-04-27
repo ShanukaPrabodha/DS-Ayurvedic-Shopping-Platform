32706cc70d8d206c7a8472e1510dd2528c9ffba8
Hasitha Sanjaya <79785680+hasitha1998@users.noreply.github.com>
2023-04-27 20:29:03 +0530

    impelemented the frontend rating orders

--- frontend/src/pages/seller-dashboard/SellerDashboard.jsx
+++ frontend/src/pages/seller-dashboard/SellerDashboard.jsx
@@ -25,7 +25,7 @@ if (currentSellerId) {
     <div className="max-w-xs">
       <div className="bg-white shadow-xl rounded-lg py-3">
         <div className="photo-wrapper p-2">
-          <img className="w-32 h-32 rounded-full mx-auto" src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="Seller's profile picture" />
+          <img className="w-32 h-32 rounded-full mx-auto" src="https://source.unsplash.com/300x300/?boy" alt="Seller's profile picture" />
         </div>
         <div className="p-2">
           <h3 className="text-center text-xl text-gray-900 font-medium leading-8">{seller.name}</h3>
@@ -40,7 +40,59 @@ if (currentSellerId) {
               </tr>
               <tr>
                 <td className="px-2 py-2 text-gray-500 font-semibold">Seller Rating</td>
-                <td className="px-2 py-2">Seller's Rating (out of 5 stars)</td>
+                <div className="flex items-center mb-3">
+					<svg
+						aria-hidden="true"
+						className="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>First star</title>
+						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+					</svg>
+					<svg
+						aria-hidden="true"
+						className="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Second star</title>
+						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+					</svg>
+					<svg
+						aria-hidden="true"
+						className="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Third star</title>
+						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+					</svg>
+					<svg
+						aria-hidden="true"
+						className="w-5 h-5 text-yellow-400"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Fourth star</title>
+						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+					</svg>
+					<svg
+						aria-hidden="true"
+						className="w-5 h-5 text-gray-300 dark:text-gray-500"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<title>Fifth star</title>
+						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+					</svg>
+					<p className="ml-2 text-sm font-medium text-gray-900 dark:text-white">4.95 out of 5</p>
+				</div>
               </tr>
               <tr>
                 <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
